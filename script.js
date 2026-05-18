a1b3efcadf82a2ffce9fdad49682c17b4ba687ff
karlijc92 <karlijc92@gmail.com>
2026-05-18 12:47:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,21 @@ function humanizeText() {
   const output = document.getElementById("outputText");
 
   if (input.trim() === "") {
+
     output.innerText = "Please paste text first.";
+
     return;
   }
 
+  let humanized = input;
+
+  humanized = humanized.replace(/\butilize\b/gi, "use");
+  humanized = humanized.replace(/\bfurthermore\b/gi, "also");
+  humanized = humanized.replace(/\bin order to\b/gi, "to");
+  humanized = humanized.replace(/\btherefore\b/gi, "so");
+  humanized = humanized.replace(/\bhowever\b/gi, "but");
+
   output.innerText =
     "Humanized Version:\n\n" +
-    input;
+    humanized;
 }
